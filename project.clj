6fa3b1dbfab049cbd247ce9f9f48213643fb8a15
Jeremy Barlow <jeremy.barlow@puppet.com>
2017-05-12 09:24:53 -0700

    (SERVER-1803) Reduce duplication between base and uberjar dependencies
    Previously, much of the base dependency content was repeated (with just
    some extra exclusions applied) in the uberjar profile.  In this commit,
    the uberjar dependencies derive directly from the base dependencies,
    with just the extra exclusions listed separately.

--- project.clj
+++ project.clj
@@ -1,6 +1,70 @@
 (def jruby-version "1.7.26")
 (def jffi-version "1.2.12")
-(def jnr-x86asm-version "1.0.2")
+
+(def base-dependencies
+  "Base set of dependencies which are built into the project's artifact.
+  Note that these dependencies are customized somewhat when building an
+  uberjar.  See the `uberjar-dependencies` variable."
+  [['org.jruby/jruby-core jruby-version
+    :exclusions ['com.github.jnr/jffi
+                 'com.github.jnr/jnr-x86asm
+                 'org.ow2.asm/asm
+                 'org.ow2.asm/asm-analysis
+                 'org.ow2.asm/asm-commons
+                 'org.ow2.asm/asm-tree
+                 'org.ow2.asm/asm-util]]
+
+   ;; jruby-core has dependencies on discrete org.ow2.asm artifacts whereas
+   ;; other common Clojure projects like core.async declare a dependency on
+   ;; org.ow2.asm/asm-all, which provides a superset of the content of the
+   ;; discrete org.ow2.asm dependencies.  Defining asm-all here allows for
+   ;; conflict resolution to be possible in consuming projects.
+   ['org.ow2.asm/asm-all "5.0.3"]
+
+   ;; jffi and jnr-x86asm are explicit dependencies because, in JRuby's poms,
+   ;; they are defined using version ranges, and :pedantic? :abort won't
+   ;; tolerate this.
+   ['com.github.jnr/jffi jffi-version]
+   ['com.github.jnr/jffi jffi-version :classifier "native"]
+   ['com.github.jnr/jnr-x86asm "1.0.2"]
+   ['org.jruby/jruby-stdlib jruby-version]])
+
+;; For the jruby-deps uberjar builds, we want to exclude a few dependencies
+;; which we expect to be provided by other jars in the Java classpath.  We
+;; do this in order to make the dependency resolution predictable regardless
+;; of the order in which the jars are referenced on the classpath.  The
+;; following variables list the dependencies which are excluded from the
+;; default ones listed in the `base-dependencies` variable above.
+
+(def extra-top-level-dependency-exclusions-from-uberjar
+  "Set of dependencies which are excluded from the top-level ones in
+  `base-dependencies` when building an uberjar.  For example, if
+  `base-dependencies` were to include [['a] ['b] ['c] ['d]] and the set returned
+  from this variable were #{'b 'c}, the top-level dependencies built into the
+  uberjar would be [['a] ['d]]."
+  #{'org.ow2.asm/asm-all})
+
+(def extra-jruby-core-exclusions-from-uberjar
+  "Set of dependencies which are added to exclusions from jruby-core in
+  `base-dependencies` when building an uberjar.  For example, if the jruby-core
+  dependency were excluding ['a 'b] in `base-dependencies` and the set
+  returned from this variable were #{'c 'd}, the complete list of dependencies
+  which would be excluded from the jruby-core dependency would be
+  ['a 'b 'c 'd]."
+  #{'joda-time
+    'org.yaml/snakeyaml})
+
+(def uberjar-dependencies
+  "Customize the list of dependencies from `base-dependencies` for use in
+  building an uberjar."
+  (->> base-dependencies
+    (remove #(contains? extra-top-level-dependency-exclusions-from-uberjar
+               (first %)))
+    (map #(if (= 'org.jruby/jruby-core (first %))
+            (update % (dec (count %))
+              (fn [exclusions]
+                (concat exclusions extra-jruby-core-exclusions-from-uberjar)))
+            (identity %)))))
 
 (defproject puppetlabs/jruby-deps "1.7.26-2-SNAPSHOT"
   :description "JRuby dependencies"
@@ -12,71 +76,16 @@
 
   :pedantic? :abort
 
-  ;; Note that there is a fair amount of duplication between the
-  ;; dependencies listed in this section and the ones in the corresponding
-  ;; section under the uberjar profile.  If you make any changes here,
-  ;; check the uberjar dependencies to see if the change should be repeated
-  ;; there.
-  :dependencies [[org.jruby/jruby-core ~jruby-version
-                  :exclusions [com.github.jnr/jffi
-                               com.github.jnr/jnr-x86asm
-                               org.ow2.asm/asm
-                               org.ow2.asm/asm-analysis
-                               org.ow2.asm/asm-commons
-                               org.ow2.asm/asm-tree
-                               org.ow2.asm/asm-util]]
-
-                 ;; jruby-core has dependencies on discrete org.ow2.asm
-                 ;; artifacts whereas other common Clojure projects like
-                 ;; core.async declare a dependency on org.ow2.asm/asm-all,
-                 ;; which provides a superset of the content of the
-                 ;; discrete org.ow2.asm dependencies.  Defining asm-all
-                 ;; here to allows for conflict resolution to be possible
-                 ;; in consuming projects.
-                 [org.ow2.asm/asm-all "5.0.3"]
-
-                 ;; jffi and jnr-x86asm are explicit dependencies because,
-                 ;; in JRuby's poms, they are defined using version ranges,
-                 ;; and :pedantic? :abort won't tolerate this.
-                 [com.github.jnr/jffi ~jffi-version]
-                 [com.github.jnr/jffi ~jffi-version :classifier "native"]
-                 [com.github.jnr/jnr-x86asm ~jnr-x86asm-version]
-                 [org.jruby/jruby-stdlib ~jruby-version]]
+  :dependencies ~base-dependencies
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/clojars_jenkins_username
                                      :password :env/clojars_jenkins_password
                                      :sign-releases false}]]
 
-  :profiles {:uberjar
-             ;; Dependencies from the project are largely repeated in the
-             ;; uberjar dependencies, with the exception that dependencies
-             ;; which are commonly used in other jar files which would be used
-             ;; with jruby-deps are omitted.  This avoids conflicts which might
-             ;; otherwise arise when multiple classpath entries on the Java
-             ;; command line provide different versions of the same dependencies.
-             ;;
-             ;; Jars which are used in conjunction with jruby-deps should
-             ;; provide their own versions of the following:
-             ;;
-             ;; [joda-time]
-             ;; [org.ow2.asm/asm-all]
-             ;; [org.yaml/snakeyaml]
-             {:dependencies ^:replace
-              [[org.jruby/jruby-core ~jruby-version
-                :exclusions [com.github.jnr/jffi
-                             com.github.jnr/jnr-x86asm
-                             joda-time
-                             org.ow2.asm/asm
-                             org.ow2.asm/asm-analysis
-                             org.ow2.asm/asm-commons
-                             org.ow2.asm/asm-tree
-                             org.ow2.asm/asm-util
-                             org.yaml/snakeyaml]]
-               [com.github.jnr/jffi ~jffi-version]
-               [com.github.jnr/jffi ~jffi-version :classifier "native"]
-               [com.github.jnr/jnr-x86asm ~jnr-x86asm-version]
-               [org.jruby/jruby-stdlib ~jruby-version]]}}
+  :profiles {:uberjar {:dependencies ~(with-meta
+                                        uberjar-dependencies
+                                        {:replace true})}}
 
   :uberjar-name "jruby-1_7.jar"
 
@@ -85,4 +94,4 @@
   ;; jar into its META-INF directory.  This is highly undesirable
   ;; for projects that already have a dependency on a different
   ;; version of bouncycastle.  Items below are excluded from the uberjar.
-  :uberjar-exclusions  [#"META-INF/jruby.home/lib/ruby/shared/org/bouncycastle"])
+  :uberjar-exclusions [#"META-INF/jruby.home/lib/ruby/shared/org/bouncycastle"])
