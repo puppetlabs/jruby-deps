9cc9deaa2ac748174e5adecca9236d96a411d2b4
Justin Stoller <justin.stoller@gmail.com>
2019-11-01 15:50:56 -0700

    (maint) Upgrade to JRuby 9.2.9.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def jruby-version "9.2.8.0")
+(def jruby-version "9.2.9.0")
 
-(defproject puppetlabs/jruby-deps "9.2.8.0-3-SNAPSHOT"
+(defproject puppetlabs/jruby-deps "9.2.9.0-1-SNAPSHOT"
   :description "JRuby dependencies"
   :url "https://github.com/puppetlabs/jruby-deps"
   :license {:name "Apache License, Version 2.0"
