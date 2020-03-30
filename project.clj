4b6d7a8fa27e7457d7ffc7be1e9a6ab7a8952f37
Maggie Dreyer <maggie@puppet.com>
2020-03-30 09:53:29 -0700

    (SERVER-2660) Update project version to match JRuby version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jruby-version "9.2.11.1")
 
-(defproject puppetlabs/jruby-deps "9.2.11.0-2-SNAPSHOT"
+(defproject puppetlabs/jruby-deps "9.2.11.1-1-SNAPSHOT"
   :description "JRuby dependencies"
   :url "https://github.com/puppetlabs/jruby-deps"
   :license {:name "Apache License, Version 2.0"
