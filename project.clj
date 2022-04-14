3896364c3d8557c7abf537043aa7d92b5a90458a
Maggie Dreyer <maggie@puppet.com>
2022-04-14 16:49:31 -0700

    (SERVER-3133) Update JRuby to 9.3.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def jruby-version "9.2.17.0")
+(def jruby-version "9.3.4.0")
 
-(defproject puppetlabs/jruby-deps "9.2.17.0-3-SNAPSHOT"
+(defproject puppetlabs/jruby-deps "9.3.4.0-1-SNAPSHOT"
   :description "JRuby dependencies"
   :url "https://github.com/puppetlabs/jruby-deps"
   :license {:name "Apache License, Version 2.0"
