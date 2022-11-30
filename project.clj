df7bb5ea18e9eb0a90927b2a82a8d9c6fffefc1b
Neil Anderson <neil.anderson@puppet.com>
2022-11-30 17:41:28 +0000

    (SERVER-3230) Bump JRuby to 9.3.9.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def jruby-version "9.3.8.0")
+(def jruby-version "9.3.9.0")
 
-(defproject puppetlabs/jruby-deps "9.3.8.0-2-SNAPSHOT"
+(defproject puppetlabs/jruby-deps "9.3.9.0-1-SNAPSHOT"
   :description "JRuby dependencies"
   :url "https://github.com/puppetlabs/jruby-deps"
   :license {:name "Apache License, Version 2.0"
