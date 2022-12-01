6246e7288266f53451b515b5657f259085c9c4d5
Jenkins CI <ci@puppet.com>
2022-12-01 16:08:06 +0000

    Version 9.3.9.0-1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jruby-version "9.3.9.0")
 
-(defproject puppetlabs/jruby-deps "9.3.9.0-1-SNAPSHOT"
+(defproject puppetlabs/jruby-deps "9.3.9.0-1"
   :description "JRuby dependencies"
   :url "https://github.com/puppetlabs/jruby-deps"
   :license {:name "Apache License, Version 2.0"
