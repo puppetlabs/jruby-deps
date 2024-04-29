ad445b26e21ed67e2514207aeb716f16ce53a1c3
Justin Stoller <justin.stoller@gmail.com>
2024-04-29 15:40:17 -0700

    (maint) Take up JRuby 9.4.7.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def jruby-version "9.4.6.0")
+(def jruby-version "9.4.7.0")
 
-(defproject puppetlabs/jruby-deps "9.4.6.0-2-SNAPSHOT"
+(defproject puppetlabs/jruby-deps "9.4.7.0-1-SNAPSHOT"
   :description "JRuby dependencies"
   :url "https://github.com/puppetlabs/jruby-deps"
   :license {:name "Apache License, Version 2.0"
